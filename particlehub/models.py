e4dbd18e1a862cc8ff8f2ee34a45463871cdd0eb
jzalger <gmail.com>
2021-01-01 00:27:10 -0500

    - Expanded log and hub manager models - Expanded main app - Added influx requirements to Pipfile

--- particlehub/models.py
+++ particlehub/models.py
@@ -1,8 +1,9 @@
 import requests
 import simplejson as json
 from string import Template
+from influxdb import InfluxDBClient
 
-BASE_URL = "https://api.particle.io/v1/"
+BASE_PARTICLE_URL = "https://api.particle.io/v1/"
 
 
 ###################################################################################################
@@ -14,7 +15,7 @@ class ParticleCloud:
         self.cloud_api_token = cloud_api_token
 
     def get_devices(self):
-        url = BASE_URL + "devices"
+        url = BASE_PARTICLE_URL + "devices"
         request = requests.get(url, params=dict(access_token=self.cloud_api_token))
         if request.status_code == requests.codes.ok:
             devices = request.json()
@@ -23,20 +24,61 @@ class ParticleCloud:
             return None
 
 
+class HubManager:
+
+    def __init__(self, cloud_api_token):
+        self.cloud = ParticleCloud(cloud_api_token)
+        self.devices = None
+        self.log_managers = None
+        self.threadpool = None
+
+        self.update_device_list()
+
+    def update_device_list(self):
+        try:
+            self.devices = self.cloud.get_devices()
+        except:
+            return None
+
+    def add_log_manager(self, device, log_source, log_credentials):
+        device.log_managed = True
+        manager = LogManager(device, log_source=log_source, log_credentials=log_credentials)
+        self.log_managers[device.device_id] = manager
+
+
+class LogManager:
+
+    def __init__(self, device, log_source="influx", log_credentials=None):
+        self.device = device
+        self.log_source = log_source
+        self.log_credentials = log_credentials
+
+    def _log_to_influx(self, new_data, tags=None):
+        point = [{"measurement": new_data["name"], "fields": {"value": new_data["result"]}}]
+        if tags is not None:
+            point[0]["tags"] = tags
+        try:
+            # InfluxDBClient(influx_host, influx_port, influx_user, influx_password, influx_db_name)
+            client = InfluxDBClient(**self.log_credentials)
+            client.write_points(point)
+        except:
+            pass
+
+
 ###################################################################################################
 # Device Classes
 
 class Device:
     DEVICE_TYPE = "unknown"
-    API_DEVICE_URL = Template(BASE_URL + "devices/$device_id/")
-    API_GET_URL = Template(BASE_URL + "devices/$device_id/$var_name")
-    API_FUNC_URL = Template(BASE_URL + "devices/$device_id/$func_name")
-    API_VITALS_URL = Template(BASE_URL + "diagnostics/$device_id/last")
+    API_DEVICE_URL = Template(BASE_PARTICLE_URL + "devices/$device_id/")
+    API_GET_URL = Template(BASE_PARTICLE_URL + "devices/$device_id/$var_name")
+    API_FUNC_URL = Template(BASE_PARTICLE_URL + "devices/$device_id/$func_name")
+    API_VITALS_URL = Template(BASE_PARTICLE_URL + "diagnostics/$device_id/last")
 
     def __init__(self, device_id, cloud_api_token, name=None, tags=None, variables=None, variable_state=None,
-                 notes=None,
-                 connected=None, online=None, status=None):
+                 notes=None, connected=None, online=None, status=None, log_managed=False):
         self.device_id = device_id
+        self.log_managed = log_managed
         self.cloud_api_token = cloud_api_token
         self.name = name
         self.tags = tags
@@ -55,12 +97,12 @@ class Device:
 
     def get_all_variable_data(self):
         for tag in self.tags:
-            tag_val = self._get_variable_data(tag)
+            tag_val = self.get_variable_data(tag)
             self.tags[tag] = tag_val["result"]
 
         for variable in self.variables:
             try:
-                self.variable_state[variable] = self._get_variable_data(variable)
+                self.variable_state[variable] = self.get_variable_data(variable)
             except requests.exceptions.RequestException:
                 # TODO: Log this failure somewhere
                 pass
@@ -70,12 +112,15 @@ class Device:
 
     def _update_variable_names(self):
         """Queries the variables available with this device"""
+        new_info = self.full_device_data()
+        self.variables = json.loads(new_info)["variables"]
+
+    def full_device_data(self):
         new_info = send_get_request(url=Device.API_DEVICE_URL.substitute(dict(device_id=self.device_id)),
                                     params=dict(access_token=self.cloud_api_token))
-        self.variables = json.loads(new_info)["variables"]
-        raise NotImplementedError
+        return json.loads(new_info)
 
-    def _get_variable_data(self, var):
+    def get_variable_data(self, var):
         """Returns the current device state as a JSON formatted string"""
         if var not in self.variables:
             return dict()
--- particlehub/particlehub_app.py
+++ particlehub/particlehub_app.py
@@ -1,30 +1,42 @@
-from flask import Flask, render_template, request, jsonify
-from models import ParticleCloud, Device
-from secrets import cloud_api_token
+from flask import Flask, render_template, request, jsonify, make_response
+from models import ParticleCloud, Device, HubManager
+from secrets import cloud_api_token, log_config, web_host
 
-# Temp variables
-web_host = '0.0.0.0'
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 cloud = ParticleCloud(cloud_api_token)
+hub_manager = HubManager(cloud_api_token)
 
 
 @app.route('/')
-def main():
+def root():
     return render_template('particlehub.html')
 
 
 @app.route('/get-devices')
 def get_devices():
     devices = cloud.get_devices()
-    return jsonify(devices)
+    return make_response(jsonify(devices), 200)
 
 
 @app.route('/get-device-info')
 def get_device_info():
-    pass
+    device_id = request.args.get('device_id')
+    device = hub_manager.devices[device_id]
+    device_info = device.full_device_data()
+    return make_response(jsonify(device_info), 200)
+
+
+@app.route('/add-device')
+def add_device():
+    log_source = request.args.get('log_source')
+    device_id = request.args.get('device_id')
+    device = hub_manager.devices[device_id]
+    log_credentials = log_config[log_source]
+    hub_manager.add_log_manager(device, log_source, log_credentials)
+    return make_response("success", 200)
 
 
 if __name__ == '__main__':
