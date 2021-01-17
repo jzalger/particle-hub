6b5d4ec2f3c9eb51acf0082d881291f0f06d3890
jzalger <gmail.com>
2021-01-16 19:57:40 -0500

    Cleaned up variable scoping issues

--- particlehub/static/particlehub.js
+++ particlehub/static/particlehub.js
@@ -18,43 +18,42 @@ $(document).ready(function($){
           });
     });
 });
-
 function update_device_table() {
   $.get("/get-devices", function(data, status){
     $('#device-list').html(data);
 
-    // TODO: This code needs to be abstracted
     // Attach detail info callbacks to the new rows.
     $(".device-row").click(function () {
         let device_id = $(this).attr('data-id');
-        $.get("/get-device-info", {"id": device_id}, function(data, status){
-            $("#device-details").html(data);
-
-            // Attach tagging callbacks to the variable rows
-            $(".tag-row").click(function (){
-                let device_id = $(this).attr('data-id');
-                let tag = $(this).attr('data-tag');
-                $.get("/add-tag", {"id": device_id, "tag": tag}, function(data, status){
-                    let row = $("button[data-tag='" + data.tag +"']");
-                    $(row).append('<i class="fas fa-tag text-info"></i>');
-                });
-            });
+        $.get("/get-device-info", {"id": device_id}, function(_data, _status){
+            $("#device-details").html(_data);
+            attach_tagging_callbacks();
         });
     });
     // Attach callbacks for add/remove
     $(".remove-device-btn").click(function(){
         let device_id = $(this).attr('data-id');
-        $.get("/remove-device", {"id": device_id}, function(data, status){
+        $.get("/remove-device", {"id": device_id}, function(_data, _status){
             update_device_table();
         });
     });
     $(".add-device-btn").click(function(){
         let device_id = $(this).attr('data-id');
         let log_source = $(this).attr('data-log-source');
-        $.get("/add-device", {"id": device_id, "log_source": log_source}, function(data, status){
+        $.get("/add-device", {"id": device_id, "log_source": log_source}, function(_data, _status){
             update_device_table();
         });
     });
   });
 }
-
+function attach_tagging_callbacks(){
+    // Attach tagging callbacks to the variable rows
+    $(".tag-row").click(function (){
+        let device_id = $(this).attr('data-id');
+        let tag = $(this).attr('data-tag');
+        $.get("/add-tag", {"id": device_id, "tag": tag}, function(data, status){
+            let row = $("button[data-tag='" + data.tag +"']");
+            $(row).append('<i class="fas fa-tag text-info"></i>');
+        });
+    });
+}
